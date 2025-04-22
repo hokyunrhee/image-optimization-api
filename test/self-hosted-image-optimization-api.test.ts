98b20df4e4f4e16286e0e0f0e9693cb4cb31d8a8
이호균 <hokyun@ihogyun-ui-MacBookAir.local>
2025-04-22 23:29:56 +0900

    Initial commit

---
+++ test/self-hosted-image-optimization-api.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SelfHostedImageOptimizationApi from '../lib/self-hosted-image-optimization-api-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/self-hosted-image-optimization-api-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SelfHostedImageOptimizationApi.SelfHostedImageOptimizationApiStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
